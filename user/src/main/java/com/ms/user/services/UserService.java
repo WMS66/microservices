d053b13605c6c0550ca5e5a60b09fb8c4dc15cd2
Wanderley Cabral da Silva <wcabraldasilvac@gmail.com>
2023-11-08 14:03:57 -0300

    :rocket: feat:implements the producer email method in UserService

--- user/src/main/java/com/ms/user/services/UserService.java
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -4,20 +4,26 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repository.UserRepository;
 
 @Service
 public class UserService {
 	
 	final UserRepository userRepository;
+	final UserProducer userProducer;
 	
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository, UserProducer userProducer) {
 		this.userRepository = userRepository;
+		this.UserProducer = userProducer;
 	}
 	
 	@Transactional
 	public UserModel save(UserModel userModel) {
-		return userRepository.save(userModel);
+		userModel = userRepository.save(userModel);
+		userProducer.publishMessageEmail(userModel);
+
+		return userModel;
 	}
 	
 
